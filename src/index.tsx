106c14f5e646962596764205c328fc0754e7b456
Timo Blomqvist <blomqvist.timo@gmail.com>
2022-05-28 17:25:11 +0300

    Add version based localStorage clear

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,13 @@ import { render } from 'solid-js/web';
 import './index.css';
 import App from './App';
 
+const version = '0.0.1';
+
+const storedVersion = localStorage.getItem('version');
+
+if (version !== storedVersion) {
+    localStorage.clear();
+    localStorage.setItem('version', version);
+}
+
 render(() => <App />, document.getElementById('root') as HTMLElement);
